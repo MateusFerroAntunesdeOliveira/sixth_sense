07a2051e71bbc0411fefab6435a25b80e416940d
Mateus <mateus.ferro.2001@hotmail.com>
2021-11-18 10:17:59 -0300

    EditJogador update

--- lib/src/pages/player/edit_page.dart
+++ lib/src/pages/player/edit_page.dart
@@ -22,7 +22,9 @@ class EditPlayer extends StatelessWidget {
         leading: IconButton(
           icon: const Icon(Icons.arrow_back),
           tooltip: "Voltar",
-          onPressed: () { Navigator.of(context).pop(); },
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
         ),
         backgroundColor: const Color(0xFF9F150D),
         toolbarHeight: 56,
@@ -32,80 +34,135 @@ class EditPlayer extends StatelessWidget {
           style: TextStyle(fontSize: 24, fontWeight: FontWeight.w500),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
-        child: Column(
-          children: [
-            //-> Imagem e alguns TextsForms
-            Row(
-              children: [
-                //-> Imagem
-                Container(
-                  height: 228,
-                  width: 128,
-                  decoration: BoxDecoration(
-                    image: const DecorationImage(
-                      image: AssetImage("assets/images/Player.png"),
-                      fit: BoxFit.cover,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+          child: Column(
+            children: [
+              //-> Imagem e alguns TextsForms
+              Row(
+                children: [
+                  //-> Imagem
+                  Expanded(
+                    flex: 2,
+                    child: Container(
+                      height: 228,
+                      width: 128,
+                      decoration: BoxDecoration(
+                        image: const DecorationImage(
+                          image: AssetImage("assets/images/Player.png"),
+                          fit: BoxFit.cover,
+                        ),
+                        border: Border.all(color: Color(0xFF9F150D), width: 2),
+                        borderRadius: BorderRadius.circular(16),
+                      ),
                     ),
-                    border: Border.all(color: Color(0xFF9F150D), width: 2),
-                    borderRadius: BorderRadius.circular(16),
                   ),
-                ),
-                //-> TextsForms
-                Column(
-                  children: [
-                    //-> Nome
-                    //-> Peso e Altura
-                    Row(
-                      children: const [],
+                  //-> TextsForms
+                  Expanded(
+                    flex: 3,
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 8),
+                      child: Column(
+                        children: [
+                          //-> Nome
+                          _inputText(hintText: "Nome"),
+                          SizedBox(height: 10),
+                          //-> Peso e Altura
+                          _inputText(hintText: "Peso"),
+                          SizedBox(height: 10),
+                          //-> Numero e Tipo de Sangue
+                          _inputText(hintText: "Numero"),
+                          SizedBox(height: 10),
+                          //-> Posição
+                          _inputText(hintText: "Posição"),
+                          //TODO Questão das rows com outros textsForms...
+                          // Expanded(flex: 1, child: _inputText(hintText: "Telefone")),
+                          // Row(
+                          //   children: [
+                          //     _inputText(hintText: "Nome"),
+                          //   ],
+                          // ),
+                        ],
+                      ),
                     ),
-                    //-> Numero e Tipo de Sangue
-                    Row(
-                      children: const [],
-                    ),
-                    //-> Posição
-                  ],
-                ),
-              ],
-            ),
-            //-> TextsForms
-            SizedBox(height: 12),
-            Row(
-              children: [
-                Expanded(
-                  child: _inputText(
-                    hintText: "Código do sensor",
                   ),
-                ),
-                SizedBox(width: 12),
-                Container(
-                  width: 84,
-                  height: 40,
-                  color: Colors.black,
-                ),
-              ],
-            ),
-            SizedBox(height: 8),
-            _inputText(hintText: "Email"),
-            SizedBox(height: 8),
-            _inputText(hintText: "Telefone"),
-            SizedBox(height: 8),
-            Row(
-              children: const [
-                Text(
-                  "CONTATO DE EMERGÊNCIA",
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
-                ),
-              ],
-            ),
-            SizedBox(height: 8),
-            _inputText(hintText: "Nome"),
-            SizedBox(height: 8),
-            _inputText(hintText: "Telefone"),
-            SizedBox(height: 8),
-            //-> Botao salvar
-          ],
+                ],
+              ),
+              //-> TextsForms
+              SizedBox(height: 12),
+              //-> Codigo do Sensor e botão Conectar
+              Row(
+                children: [
+                  Expanded(
+                    child: _inputText(
+                      hintText: "Código do sensor",
+                    ),
+                  ),
+                  SizedBox(width: 12),
+                  ElevatedButton(
+                    style: BasicButtonStyle(
+                      elevation: 0,
+                      backgroundColor: Colors.transparent,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(4),
+                        side: BorderSide(color: Color(0xFFBDBDBD)),
+                      ),
+                    ),
+                    child: Text(
+                      "CONECTAR",
+                      style: TextStyle(color: Color(0xFFBDBDBD), fontSize: 16),
+                    ),
+                    onPressed: () {
+                      //TODO OnPressed do botão Salvar
+                    },
+                  ),
+                ],
+              ),
+              SizedBox(height: 8),
+              _inputText(hintText: "Email"),
+              SizedBox(height: 8),
+              _inputText(hintText: "Telefone"),
+              SizedBox(height: 8),
+              //-> Contato de Emergência
+              Row(
+                children: const [
+                  Text(
+                    "CONTATO DE EMERGÊNCIA",
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+                  ),
+                ],
+              ),
+              SizedBox(height: 8),
+              _inputText(hintText: "Nome"),
+              SizedBox(height: 8),
+              _inputText(hintText: "Telefone"),
+              SizedBox(height: 8),
+              //-> Botao salvar
+              Row(
+                children: [
+                  Expanded(
+                    child: ElevatedButton(
+                      style: BasicButtonStyle(
+                        elevation: 0,
+                        backgroundColor: Color(0xFF0A7C00),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(4),
+                        ),
+                      ),
+                      child: Text(
+                        "SALVAR",
+                        style: TextStyle(color: Colors.white, fontSize: 16),
+                      ),
+                      onPressed: () {
+                        //TODO OnPressed do botão Salvar
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/src/utils/styles.dart
+++ lib/src/utils/styles.dart
@@ -1,5 +1,49 @@
 import 'package:flutter/material.dart';
 
+class BasicButtonStyle extends ButtonStyle {
+  BasicButtonStyle({
+    TextStyle? textStyle,
+    Color? backgroundColor,
+    Color? disabledColor,
+    Color? foregroundColor,
+    Color? overlayColor,
+    Color? shadowColor,
+    double? elevation,
+    EdgeInsets? padding,
+    Size? minimumSize,
+    BorderSide? side,
+    OutlinedBorder? shape,
+    MouseCursor? mouseCursor,
+    VisualDensity? visualDensity,
+    MaterialTapTargetSize? tapTargetSize,
+    Duration? animationDuration,
+    bool? enableFeedback,
+    Alignment? alignment,
+  }) : super(
+          textStyle: MaterialStateProperty.all(textStyle),
+          backgroundColor: MaterialStateProperty.resolveWith((states) {
+            if (disabledColor != null && states.contains(MaterialState.disabled)) {
+              return disabledColor;
+            }
+            return backgroundColor;
+          }),
+          foregroundColor: MaterialStateProperty.all(foregroundColor),
+          overlayColor: MaterialStateProperty.all(overlayColor),
+          shadowColor: MaterialStateProperty.all(shadowColor),
+          elevation: MaterialStateProperty.all(elevation),
+          padding: MaterialStateProperty.all(padding),
+          minimumSize: MaterialStateProperty.all(minimumSize),
+          side: MaterialStateProperty.all(side),
+          shape: MaterialStateProperty.all(shape),
+          mouseCursor: MaterialStateProperty.all(mouseCursor),
+          visualDensity: visualDensity,
+          tapTargetSize: tapTargetSize,
+          animationDuration: animationDuration,
+          enableFeedback: enableFeedback,
+          alignment: alignment,
+        );
+}
+
 class SixthSenseStyles {
   static InputDecoration inputDecoration(
     String hintText, {
